99484984a1503cf377d69e7e51bc5793bbbfde06
Emilia Heller-Mrotek <emkacf@gmail.com>
2023-04-16 16:15:06 +0200

    chore: route to home

--- libs/shared/src/lib/components/top-nav/top-nav.component.ts
+++ libs/shared/src/lib/components/top-nav/top-nav.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '@pet-identity/ui-kit';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'shared-top-nav',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, RouterLink],
   templateUrl: './top-nav.component.html',
   styleUrls: ['./top-nav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
