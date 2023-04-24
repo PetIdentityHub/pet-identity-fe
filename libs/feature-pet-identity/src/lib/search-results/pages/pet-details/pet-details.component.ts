4635cc59c1e742b6634374885771f5ced49f2f90
Emilia Heller-Mrotek <emkacf@gmail.com>
2023-04-24 22:26:28 +0200

    chore: styling changes

--- libs/feature-pet-identity/src/lib/search-results/pages/pet-details/pet-details.component.ts
+++ libs/feature-pet-identity/src/lib/search-results/pages/pet-details/pet-details.component.ts
@@ -18,12 +18,12 @@ export class PetDetailsComponent implements OnInit {
   getPet$!: Observable<Pet | undefined>;
 
   constructor(private petsService: PetsService, private route: ActivatedRoute) {
-    //this.getPet$ = this.petsService.getPetMetadataByChipNumber(this.route.snapshot.params['id']);
+    this.getPet$ = this.petsService.getMockPetMetadata(this.route.snapshot.params['id']);
   }
 
   ngOnInit(): void {
-    this.petsService.getPetMetadataByChipNumber('PetChipId').subscribe((res) => {console.log(res)});
-    this.petsService.getPetMetadataByName('PetName').subscribe((res) => {console.log(res)});
+    //this.petsService.getPetMetadataByChipNumber('PetChipId').subscribe((res) => {console.log(res)});
+    //this.petsService.getPetMetadataByName('PetName').subscribe((res) => {console.log(res)});
   }
 
 }
